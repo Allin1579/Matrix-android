75775a6d6446ed0aefe544d79d7d120385e87a73
Allin <liyu.09@foxmail.com>
2016-06-26 15:29:07 +0800

    ...

---
+++ app/src/main/java/com/allin/matrix/ui/activity/LaunchActivity.java
@@ -0,0 +1,15 @@
+package com.allin.matrix.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.allin.matrix.R;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
