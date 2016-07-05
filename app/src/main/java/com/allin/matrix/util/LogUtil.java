a5afa54eee941737716097348aef2bd9dc8734b9
Allin <liyu.09@foxmail.com>
2016-07-05 14:17:39 +0800

    ...

--- app/src/main/java/com/allin/matrix/util/LogUtil.java
+++ app/src/main/java/com/allin/matrix/util/LogUtil.java
@@ -2,7 +2,7 @@ package com.allin.matrix.util;
 
 import android.util.Log;
 
-import com.allin.matrix.Config;
+import com.allin.matrix.BuildConfig;
 
 /**
  * Created by Allin on 2016/6/20.
@@ -11,47 +11,47 @@ public class LogUtil {
     private static final int N = 0;
 
     public static int v(String tag, String msg) {
-        return Config.DEBUG ? Log.v(tag, msg) : N;
+        return BuildConfig.DEBUG ? Log.v(tag, msg) : N;
     }
 
     public static int v(String tag, String msg, Throwable tr) {
-        return Config.DEBUG ? Log.v(tag, msg, tr) : N;
+        return BuildConfig.DEBUG ? Log.v(tag, msg, tr) : N;
     }
 
     public static int d(String tag, String msg) {
-        return Config.DEBUG ? Log.d(tag, msg) : N;
+        return BuildConfig.DEBUG ? Log.d(tag, msg) : N;
     }
 
     public static int d(String tag, String msg, Throwable tr) {
-        return Config.DEBUG ? Log.d(tag, msg, tr) : N;
+        return BuildConfig.DEBUG ? Log.d(tag, msg, tr) : N;
     }
 
     public static int i(String tag, String msg) {
-        return Config.DEBUG ? Log.i(tag, msg) : N;
+        return BuildConfig.DEBUG ? Log.i(tag, msg) : N;
     }
 
     public static int i(String tag, String msg, Throwable tr) {
-        return Config.DEBUG ? Log.i(tag, msg, tr) : N;
+        return BuildConfig.DEBUG ? Log.i(tag, msg, tr) : N;
     }
 
     public static int w(String tag, String msg) {
-        return Config.DEBUG ? Log.w(tag, msg) : N;
+        return BuildConfig.DEBUG ? Log.w(tag, msg) : N;
     }
 
     public static int w(String tag, String msg, Throwable tr) {
-        return Config.DEBUG ? Log.w(tag, msg, tr) : N;
+        return BuildConfig.DEBUG ? Log.w(tag, msg, tr) : N;
     }
 
     public static int w(String tag, Throwable tr) {
-        return Config.DEBUG ? Log.w(tag, tr) : N;
+        return BuildConfig.DEBUG ? Log.w(tag, tr) : N;
     }
 
     public static int e(String tag, String msg) {
-        return Config.DEBUG ? Log.e(tag, msg) : N;
+        return BuildConfig.DEBUG ? Log.e(tag, msg) : N;
     }
 
     public static int e(String tag, String msg, Throwable tr) {
-        return Config.DEBUG ? Log.e(tag, msg, tr) : N;
+        return BuildConfig.DEBUG ? Log.e(tag, msg, tr) : N;
     }
 
 }
